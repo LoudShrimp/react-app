4704873cb88e6284ccb88f32ef6504eb9aea9b11
LoudShrimp <[mphapakdy@gmail.com]>
2023-11-22 15:35:27 -0800

    test changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Alert from "./components/Alert";
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
+import ListGroup from "./components/ListGroup";
 import { useState } from "react";
+import "./App.css";
+import { FaCalendarAlt } from "react-icons/fa";
+import Like from "./components/Like";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
+  let items = ["New York", "San Francisco", "Tokyo"];
 
   return (
     <div>
@@ -13,6 +18,15 @@ function App() {
       <Button color="primary" onClick={() => setAlertVisibility(true)}>
         My Button
       </Button>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={function (item: string): void {
+          throw new Error("Function not implemented.");
+        }}
+      />
+      <FaCalendarAlt color="red" size="40" />
+      <Like onClick={() => console.log("clicked")} />
     </div>
   );
 }
