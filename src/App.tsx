42cf992ef05b321b6044797d6430190fe0e75e84
LoudShrimp <PestLines@gmail.com>
2023-11-30 14:29:48 -0800

    more changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,26 @@ import NavBar from "./components/NavBar";
 import Cart from "./components/Cart";
 import ExpandableText from "./components/ExpandableText";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const [alertVisible, setAlertVisibility] = useState(false);
   const [cartItems, setCartItems] = useState(["Product1", "Product2"]);
+
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
 
   //Updating State exercise 1:
   // const [game, setGame] = useState({
@@ -88,7 +104,25 @@ function App() {
         fugiat magnam natus officia voluptatibus accusamus inventore, assumenda
         cum doloribus provident expedita, quis optio ullam?
       </ExpandableText>
-      <Form />
+      <div className="mb-3">
+        <Form />
+      </div>
+      <div className="mb-5">
+        <ExpenseForm
+          onSubmit={(expense) =>
+            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
+          }
+        />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
     </div>
   );
 }
